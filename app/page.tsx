c71c1bf18fcf22ce1662075398a0b9160bd284e6
anjali100-coder <manishunichem@gmail.com>
2026-04-24 00:27:28 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,8 @@
-export const dynamic ='force-dynamic'
-export const revalidate = 0
-import { Header } from "@/components/header"
-import { HeroSection } from "@/components/hero-section"
-import { AboutSection } from "@/components/about-section"
-import { ServicesSection } from "@/components/services-section"
-import { TeamSection } from "@/components/team-section"
-import { TestimonialsSection } from "@/components/testimonials-section"
-import { ContactSection } from "@/components/contact-section"
-import { Footer } from "@/components/footer"
-
-
-
-
-export default function Home() {
-  
-
-  return (
-    <main className="min-h-screen">
-      <Header />
-      <HeroSection />
-      <AboutSection />
-      <ServicesSection />
-      <TeamSection  />
-      <TestimonialsSection testimonials={testimonials} />
-      <ContactSection  />
-      <Footer />
-    </main>
-  )
-}
+import {HeroSection } from "@/components/hero-section"
+ export default function Home() {
+   return(
+     <main className="min-h-screen">
+       <HeroSection />
+     </main>
+     )
+ }
