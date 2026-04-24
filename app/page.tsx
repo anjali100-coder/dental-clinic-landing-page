7d627f603ee55fd8a9c9d9d4145a7374ed500fd1
anjali100-coder <manishunichem@gmail.com>
2026-04-24 16:39:49 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
                   <p className="font-bold text-gray-900 text-lg">Full Protection</p>
                   <p className="text-sm text-gray-600">Shield against infections</p>
                 </div>
-              </div>
               <div className="bg-white rounded-2xl p-5 flex items-center gap-4 shadow-2xl">
                 <div className="bg-blue-100 p-3 rounded-xl text-2xl">👍</div>
                 <div>
@@ -41,7 +40,7 @@ export default function Home() {
           </div>
         </div>
       </div>
-      )
-      }
+        )
+        }
     </div>
   )
