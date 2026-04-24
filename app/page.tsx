9b7c8f3ed2956c93fea63875ec1c942da3fe5ca0
anjali100-coder <manishunichem@gmail.com>
2026-04-24 11:02:18 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import {HeroSection } from "@/components/hero-section"
+import {AboutSection } from "@/components/hero-section"
+
  export default function Home() {
    return(
      <main className="min-h-screen">
        <HeroSection />
+      <AboutSection />
      </main>
      )
  }
